cde856fa84f8b53300e50524537a81cdeb130f85
Mehebul02 <mehebul2123@gmail.com>
2024-07-18 14:53:27 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+  
+  return (
+    <main className="container">
+     <h1 className="text-2xl text-white text-center font-serif font-semibold p-10 border-b-2 ">Css Animation</h1>
+     <div className="cardBox">
+      <div>
+
+      </div>
+     </div>
+    </main>
+  )
+}
+
+export default App
